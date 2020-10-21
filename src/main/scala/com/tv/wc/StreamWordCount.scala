774c42dc30a001bbb65a63f51f19e8589dd758a9
hubo7 <hubo7@le.com>
2020-10-21 11:01:27 +0800

    init

---
+++ src/main/scala/com/tv/wc/StreamWordCount.scala
@@ -0,0 +1,32 @@
+package com.tv.wc
+
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.scala._
+
+
+object StreamWordCount {
+  def main(args: Array[String]): Unit ={
+
+    val params = ParameterTool.fromArgs(args)
+    val host: String = params.get("host")
+    val port: Int = params.getInt("port")
+
+    // 创建一个流式处理执行环境
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val textDataSteam = env.socketTextStream(host,port)
+
+    // 逐一读取数据，打散之后进行 word count
+    val wordCountDataStream = textDataSteam.flatMap(_.split("\\s"))
+      .filter(_.nonEmpty)
+      .map((_,1))
+      .keyBy(0)
+      .sum(1)
+
+    // 打印输出
+    wordCountDataStream.print()
+
+    // 执行任务
+    env.execute("stream word count job")
+  }
+
+}
